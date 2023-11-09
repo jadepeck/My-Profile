34ca99b829d0e7f329ad33f2bb8c4c8d2aa737a8
pj taron <jadepeck27@gmail.com>
2023-11-09 23:46:24 +0900

    home4

--- src/compponents/home/Home.jsx
+++ src/compponents/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import "./home.css";
+import Me from "../../assets/avatar-1.svg";
 
 const Home = () => {
   return (
     <section className="home container" id="home">
-
+      <div className="intro">
+        <img src={Me} alt="" className="home__img" />
+      </div>
     </section>
   )
 }
